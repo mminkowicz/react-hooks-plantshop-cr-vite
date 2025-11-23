2e89a438dca990d3d07ae9884469477217e18ec8
Mendel Minkowicz <mminkowicz@gmail.com>
2025-11-22 19:49:23 -0500

    Lab Done

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data))
+      .catch((err) => console.error("Error fetching plants:", err));
+  }, []);
+
+  
+  function handleAddPlant(newPlant) {
+    setPlants((prev) => [...prev, newPlant]);
+  }
+
+  
+  function handleSearchChange(term) {
+    setSearchTerm(term);
+  }
+
+ 
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearch={handleSearchChange} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
